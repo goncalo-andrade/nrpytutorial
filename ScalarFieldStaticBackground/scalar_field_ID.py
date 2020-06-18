b8caaea7b622f4a34beb816da09edfcf0ac16847
Gonçalo Andrade <goncalo.j.c.andrade@tecnico.ulisboa.pt>
2020-06-18 15:43:45 +0100

    changed back the minus sign in testing id

--- ScalarFieldStaticBackground/scalar_field_ID.py
+++ ScalarFieldStaticBackground/scalar_field_ID.py
@@ -139,8 +139,8 @@ def scalar_field_test_ID():
     Pi_t = sp.diff(Phi_t, t)
     for i in range(DIM):
         Pi_t -= betaU[i] * sp.diff(Phi_t, coords[i])
-    Pi = 1/alpha * Pi_t.subs(t, sp.sympify(0))     # CHANGE TO A MINUS SIGN BEFORE 1/ALPHA
-
+    Pi = - 1/alpha * Pi_t.subs(t, sp.sympify(0))
+    
     # This function replaces the spherical coordinates r, th, ph for the numerical grid xx0, xx1, xx2
     # Taken from BSSN.ADM_Exact_Spherical_or_Cartesian_to_BSSNCurvilinear.py
     def sympify_integers__replace_rthph_or_Cartxyz(obj, rthph_or_xyz, rthph_or_xyz_of_xx):
