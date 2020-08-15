1b3df63f5bfcb7fab084c75896a2cc36da72d9e7
Gonçalo Andrade <goncalo.j.c.andrade@tecnico.ulisboa.pt>
2020-08-15 20:48:35 +0100

    everything up to the main c code

---
+++ BSSN_LinearScalarFieldEvolution_Ccodes/free_parameters.h
@@ -0,0 +1,14 @@
+
+// Set Black Hole mass and spin
+params.M = {M};
+params.chi = {chi};
+    
+// Set free-parameter values.
+
+const REAL domain_size    = 10;
+const REAL sinh_width     = 0.4;
+const REAL sinhv2_const_dr= 0.05;
+const REAL SymTP_bScale   = 0.5;
+
+params.RMAX = domain_size;
+
