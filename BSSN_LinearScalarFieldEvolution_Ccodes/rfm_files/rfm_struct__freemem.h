a7d32dc167124f97271d0705cc75d72c4cff3132
Gonçalo Andrade <goncalo.j.c.andrade@tecnico.ulisboa.pt>
2020-11-02 16:08:49 +0000

    deleted c codes folder

--- BSSN_LinearScalarFieldEvolution_Ccodes/rfm_files/rfm_struct__freemem.h
+++
@@ -1,7 +0,0 @@
-free(rfmstruct.f0_of_xx0);
-free(rfmstruct.f0_of_xx0__D0);
-free(rfmstruct.f0_of_xx0__DD00);
-free(rfmstruct.f0_of_xx0__DDD000);
-free(rfmstruct.f1_of_xx1);
-free(rfmstruct.f1_of_xx1__D1);
-free(rfmstruct.f1_of_xx1__DD11);
